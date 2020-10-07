95eb7c92ed395d057513cef4e8df483659f6867a
Gilad Barnea <giladbrn@gmail.com>
2020-10-07 15:55:58 +0300

    ultratb is imported only if IGIT_EXCEPTHOOK == 'VerboseTB', and ipdb is imported only if IGIT_DEBUGGER == 'ipdb' -> faster startup time

--- igit/__init__.py
+++ igit/__init__.py
@@ -1,5 +1,4 @@
 import os
-from functools import partial
 
 from time import perf_counter
 
@@ -28,6 +27,7 @@ def extend_print():
     def cprint(_colorfn, *args, **kwargs):
         print(_colorfn(*args, **kwargs))
     
+    from functools import partial
     for colorfn_name in filter(lambda attr: not str(attr).startswith('__'), dir(more_termcolor.colors)):
         colorfn = getattr(more_termcolor.colors, colorfn_name)
         
@@ -40,37 +40,47 @@ def extend_print():
 def do_debug_patching():
     import sys
     
-    from IPython.core import ultratb
-    from ipdb import set_trace
-    
     from igit.util.misc import is_pycharm
     
     # TODO: learn how to call pdb on exception just using ipdb, not relying on ipython
     #  (currently, whenever calling ipdb, it uses ipython → startup scripts → igit)
-    #  this will enable having env vars IGIT_CALL_PDB=TRUE and IGIT_MODE="" and still
+    #  this will enable having env vars IGIT_CALL_PDB_ON_EXC=TRUE and IGIT_MODE="" and still
     #  calling pdb on exception (right now they're dependent)
-    IGIT_CALL_PDB = eval(os.environ.get('IGIT_CALL_PDB', ''))
+    IGIT_CALL_PDB_ON_EXC = eval(os.environ.get('IGIT_CALL_PDB_ON_EXC', ''))
     IGIT_EXCEPTHOOK = os.environ.get('IGIT_EXCEPTHOOK', '')
+    IGIT_DEBUGGER = os.environ.get('IGIT_DEBUGGER', '')
     using_pycharm = is_pycharm()
-    actually_call_pdb = IGIT_CALL_PDB and not using_pycharm
+    actually_call_pdb = IGIT_CALL_PDB_ON_EXC and not using_pycharm
     
-    logger.info('IGIT_MODE == DEBUG',
-                'IGIT_CALL_PDB:', IGIT_CALL_PDB,
+    logger.info('IGIT_MODE == DEBUG (inside do_debug_patching())',
+                'IGIT_CALL_PDB_ON_EXC:', IGIT_CALL_PDB_ON_EXC,
                 'actually calling pdb:', actually_call_pdb,
                 'IGIT_EXCEPTHOOK:', IGIT_EXCEPTHOOK,
+                'IGIT_DEBUGGER:', IGIT_DEBUGGER,
                 'using_pycharm:', using_pycharm)
     
     if IGIT_EXCEPTHOOK == 'VerboseTB':
+        from IPython.core import ultratb
         sys.excepthook = ultratb.VerboseTB(include_vars=True, call_pdb=actually_call_pdb)
+    elif actually_call_pdb:
+        raise EnvironmentError((f"env mismatch: 'IGIT_EXCEPTHOOK' is bad and 'actually_call_pdb' is True"
+                                f"which means IGIT_CALL_PDB_ON_EXC is True and we're not using pycharm"
+                                'whether to call pdb on exception is depndent on patching excepthook currently'
+                                f'IGIT_EXCEPTHOOK: {repr(IGIT_EXCEPTHOOK)}'))
     
     if actually_call_pdb:
-        sys.breakpointhook = partial(set_trace, context=30)
+        if IGIT_DEBUGGER == 'ipdb':
+            from ipdb import set_trace
+            from functools import partial
+            sys.breakpointhook = partial(set_trace, context=30)
+        elif IGIT_DEBUGGER:
+            raise EnvironmentError(f"env: IGIT_DEBUGGER is something but not ipdb: {repr(IGIT_DEBUGGER)}")
 
 
-ts3 = perf_counter()
-extend_print()
-ts4 = perf_counter()
-print(f'igit.__init__.py | extend_print() took {round((ts4 - ts3) * 1000, 2)}ms')
+# ts3 = perf_counter()
+# extend_print()
+# ts4 = perf_counter()
+# print(f'igit.__init__.py | extend_print() took {round((ts4 - ts3) * 1000, 2)}ms')
 
 IGIT_MODE = os.environ.get('IGIT_MODE', '')
 logger.debug(f'IGIT_MODE: {repr(IGIT_MODE)}')
